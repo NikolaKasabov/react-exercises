f720a1d04f7362bddc6870ee28d3f2c957f50ad4
Nikola <n_kasabov@mail.bg>
2022-07-15 12:38:21 +0300

    exercise 4 initial commit

---
+++ 2. John Smilga course/04-accordion/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
+
+function App() {
+  const [questions, setQuestions] = useState(data);
+
+  return <main>
+
+  </main>;
+}
+
+export default App;
