73215debfb1d069c5712b9439d0a5bd4de5b834a
Nikola <n_kasabov@mail.bg>
2022-07-15 13:21:09 +0300

    exercise 4 completed

--- 2. John Smilga course/04-accordion/src/App.js
+++ 2. John Smilga course/04-accordion/src/App.js
@@ -6,7 +6,14 @@ function App() {
   const [questions, setQuestions] = useState(data);
 
   return <main>
-
+    <div className="container">
+      <h3>questions and answers about login</h3>
+      <section className="info">
+        {questions.map(question => {
+          return <SingleQuestion key={question.id} question={question} />
+        })}
+      </section>
+    </div>
   </main>;
 }
 
--- 2. John Smilga course/04-accordion/src/Question.js
+++ 2. John Smilga course/04-accordion/src/Question.js
@@ -1,8 +1,28 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ question }) => {
+  const [isShownInfo, setIsShownInfo] = useState(true);
+
+  function toggleInfoHandler() {
+    setIsShownInfo(prev => !prev);
+  }
+
+  return <article className="question">
+    <header>
+      <h4>{question.title}</h4>
+      <button className="btn" onClick={toggleInfoHandler}>
+        {isShownInfo
+          ? <AiOutlineMinus />
+          : <AiOutlinePlus />
+        }
+      </button>
+    </header>
+
+    {isShownInfo && <p>
+      {question.info}
+    </p>}
+  </article>;
 };
 
 export default Question;
