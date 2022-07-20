7c8911d6f2388acd21fea997bdea23a273f11efa
Nikola <n_kasabov@mail.bg>
2022-07-20 14:11:30 +0300

    exercise 9 completed

--- 2. John Smilga course/09-color-generator/src/App.js
+++ 2. John Smilga course/09-color-generator/src/App.js
@@ -1,10 +1,49 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
 
-import Values from 'values.js'
+import Values from 'values.js';
 
 function App() {
-  return <h2>color generator project</h2>
+  const [input, setInput] = useState('');
+  const [hasError, setHasError] = useState(false);
+  const [colors, setColors] = useState([]);
+
+  function submitHandler(ev) {
+    ev.preventDefault();
+
+    try {
+      setHasError(false);
+      const color = new Values(input);
+      const colors = color.all(10);
+      setColors(colors);
+    } catch (error) {
+      setHasError(true);
+      setColors([]);
+    }
+  }
+
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={submitHandler}>
+          <input type="text"
+            placeholder='#2d45fa'
+            className={hasError ? 'error' : null}
+            value={input}
+            onChange={(ev) => setInput(ev.target.value)}
+          />
+          <button type="submit" className='btn'>submit</button>
+        </form>
+      </section>
+
+      <section className="colors">
+        {colors.map(color => {
+          return <SingleColor key={color.hex} color={color} />
+        })}
+      </section>
+    </>
+  );
 }
 
 export default App
--- 2. John Smilga course/09-color-generator/src/SingleColor.js
+++ 2. John Smilga course/09-color-generator/src/SingleColor.js
@@ -1,8 +1,31 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from 'react';
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({ color }) => {
+  const [showMessage, setShowMessage] = useState(false);
+
+  useEffect(() => {
+    if (showMessage) {
+      setTimeout(() => { 
+        setShowMessage(false);
+      }, 3000);
+    }
+  }, [showMessage]);
+  
+  function clickHandler() {
+    navigator.clipboard.writeText('#' + color.hex);
+    setShowMessage(true);
+  }
+  
+  return (
+    <article className={`color ${color.type === 'shade' ? 'color-light' : null}`}
+      style={{ backgroundColor: '#' + color.hex }}
+      onClick={clickHandler}
+    >
+      <p className="percent-value">{color.weight}%</p>
+      <p className="color-value">#{color.hex}</p>
+      {showMessage && <p className="alert">copied to clipboard</p>}
+    </article>
+  );
 }
 
 export default SingleColor
