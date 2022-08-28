b5571c0b883f9225fbc51182db3d3b8b73bebc16
Nikola <n_kasabov@mail.bg>
2022-08-28 16:22:29 +0300

    add products reducers

--- 2. John Smilga course/comfy-sloth/src/App.js
+++ 2. John Smilga course/comfy-sloth/src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import { Navbar, Sidebar, Footer } from './components';
+import { fetchProductsThunk } from './store/productsSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    // dispatch(fetchProductsThunk());
+  }, [dispatch]);
+
   return <>
     <BrowserRouter>
       <Navbar />
--- 2. John Smilga course/comfy-sloth/src/store/productsSlice.js
+++ 2. John Smilga course/comfy-sloth/src/store/productsSlice.js
@@ -1,13 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
+export const fetchProductsThunk = createAsyncThunk(
+  'products/fetchProducts',
+  (arg, thunkAPI) => {
+    return axios.get('https://course-api.com/react-store-products')
+      .then(res => {
+        return res.data;
+      });
+  }
+);
 
 const productsSlice = createSlice({
   name: 'products',
   initialState: {
     products: [],
+    isLoading: false,
+    error: null,
   },
   reducers: {},
-  extraReducers: {}
+  extraReducers: {
+    [fetchProductsThunk.pending]: (state, action) => { 
+      state.isLoading = true;
+      state.error = null;
+    },
+    [fetchProductsThunk.fulfilled]: (state, action) => { 
+      state.isLoading = false;
+      state.products = action.payload;
+    },
+    [fetchProductsThunk.rejected]: (state, action) => { 
+      state.isLoading = false;
+      state.error = action.error?.message;
+    },
+  }
 });
 
 export const productsActions = productsSlice.actions;
