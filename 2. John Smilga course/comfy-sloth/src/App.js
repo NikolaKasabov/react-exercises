7a2bb997259339234d40e6eb260dfebf3e071e48
Nikola <n_kasabov@mail.bg>
2022-08-29 23:06:43 +0300

    add footer

--- 2. John Smilga course/comfy-sloth/src/App.js
+++ 2. John Smilga course/comfy-sloth/src/App.js
@@ -21,9 +21,10 @@ function App() {
 
       <Routes>
         <Route path='/' element={<HomePage />} />
-        {/* <Route path='about' element={<AboutPage />} /> */}
-        <Route path='about' element={false ? <AboutPage /> : <Navigate to='/' />} />
+        <Route path='about' element={<AboutPage />} />
       </Routes>
+
+      <Footer />
     </BrowserRouter>
   </>;
 }
