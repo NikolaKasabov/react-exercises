2601ed5093253da1821528d29dc805d135938728
Nikola <n_kasabov@mail.bg>
2022-11-02 06:57:57 +0200

    Progress Steps completed

--- 3.50_projects_in_50_days_course/src/App.js
+++ 3.50_projects_in_50_days_course/src/App.js
@@ -1,10 +1,15 @@
 import './App.scss';
 import ExpandingCards from './Components/1/ExpandingCards';
+import ProgressSteps from './Components/2/ProgressSteps';
 
 function App() {
   return (
     <div className="App">
+      <h2>1. Expanding Cards</h2>
       <ExpandingCards />
+      <hr />
+      <h2>2. Progress Steps</h2>
+      <ProgressSteps steps={5} />
       <hr />
     </div>
   );
--- 3.50_projects_in_50_days_course/src/Components/2/ProgressSteps.js
+++ 3.50_projects_in_50_days_course/src/Components/2/ProgressSteps.js
@@ -2,12 +2,55 @@ import { useState } from 'react';
 
 import './ProgressSteps.scss';
 
+function calcInnerLineWidth(steps, activeStep) {
+  const lineSteps = steps - 1;
+  const oneStepWidth = 100 / lineSteps;     // in percents
+  return (activeStep - 1) * oneStepWidth + '%';
+}
+
 function ProgressSteps({ steps }) {
-  const [activeStep, setActiveStep] = useState(0);
+  const [activeStep, setActiveStep] = useState(4);
+
+  const circles = [];
+
+  for (let i = 1; i <= steps; i++) {
+    circles.push((
+      <div className={`circle ${i <= activeStep ? 'active' : ''}`}>{i}</div>
+    ));
+  }
+
+  function prevClickHandler() {
+    setActiveStep(prevValue => prevValue <= 1 ? 1 : prevValue - 1);
+  }
+
+  function nextClickHandler() {
+    setActiveStep(prevValue => prevValue >= steps ? steps : prevValue + 1);
+  }
 
   return (
     <div className='progressSteps'>
-      <div className="line"></div>
+      <div className='lineContainer'>
+        <div className='line'>
+          <div className='innerLine'
+            style={{ width: calcInnerLineWidth(steps, activeStep) }}
+          ></div>
+        </div>
+
+        <div className='circlesContainer'>
+          {circles}
+        </div>
+      </div>
+
+      <div className='buttons'>
+        <button className='button'
+          onClick={prevClickHandler}
+          disabled={activeStep === 1}
+        >prev</button>
+        <button className='button'
+          onClick={nextClickHandler}  
+          disabled={activeStep === steps}
+        >next</button>
+      </div>
     </div>
   );
 }
